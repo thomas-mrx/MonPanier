51bae3d34b9f3097dae5e7bdbbbda9396326d9c4
thomas-mrx <thomas.moreaux@icloud.com>
2023-03-21 16:03:52 +0100

    fix: change db naming

--- MonPanier/settings.py
+++ MonPanier/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'remote': {
+    'production': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
