3e9a777c1755379b5eec745db5021b88909686f0
thomas-mrx <thomas.moreaux@icloud.com>
2023-03-26 13:36:00 +0200

    fix: change html title and remove annoying pop-up alert

--- MonPanier/app/src/index.ts
+++ MonPanier/app/src/index.ts
@@ -1,7 +1,10 @@
 import './style/style.scss';
 
-console.log('Hello World !');
-
 const sum = (a: number, b:number) => a + b;
 
-alert(sum(1, 8 + 2));
+// eslint-disable-next-line no-console
+console.log(sum(1, 8 + 2));
+
+window.onload = () => {
+  document.querySelector('h1').innerHTML = 'Hello World from TypeScript (index.ts)!';
+};
