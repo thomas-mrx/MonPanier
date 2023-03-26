b350133ce6e640eea2169306a3929bb3b417f9d9
thomas-mrx <thomas.moreaux@icloud.com>
2023-03-26 21:30:16 +0200

    refactor: change variable name in index.ts

--- MonPanier/app/src/index.ts
+++ MonPanier/app/src/index.ts
@@ -10,8 +10,8 @@ console.log(sum(1, 7 + 2));
 window.onload = () => {
   document.querySelector('h1').innerHTML = 'hello';
 
-  const api = new Api();
-  api.api.searchProduct('nu').then((data) => {
+  const MonPanier = new Api();
+  MonPanier.api.searchProduct('nu').then((data) => {
     // eslint-disable-next-line no-console
     console.log(data);
   });
