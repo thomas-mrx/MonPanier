e3cb87375e786e0df4699d6fa0abd1a1dd1ba686
thomas-mrx <thomas.moreaux@icloud.com>
2023-03-26 18:48:27 +0200

    fix: improve ignore pattern eslint

--- MonPanier/app/src/index.ts
+++ MonPanier/app/src/index.ts
@@ -6,5 +6,5 @@ const sum = (a: number, b:number) => a + b;
 console.log(sum(1, 8 + 2));
 
 window.onload = () => {
-  document.querySelector('h1').innerHTML = 'Hello World from TypeScript (index.ts)!';
+  document.querySelector('h1').innerHTML = 'hello';
 };
