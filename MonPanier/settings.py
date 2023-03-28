2912b744c0ce2b406737edffdb044f0def47a428
thomas-mrx <thomas.moreaux@icloud.com>
2023-03-28 17:12:50 +0200

    fix: change charset of prod. database

--- MonPanier/settings.py
+++ MonPanier/settings.py
@@ -127,6 +127,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
         'OPTIONS': {
           'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+           'charset': "utf8mb4"
         },
     }
 }
