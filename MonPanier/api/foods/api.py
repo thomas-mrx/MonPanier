893ebdbe371bbebe27dd9f4226059169fa9f0151
thomas-mrx <thomas.moreaux@icloud.com>
2023-03-31 09:33:47 +0200

    feat: add popularity order for more relevant search results

--- MonPanier/api/foods/api.py
+++ MonPanier/api/foods/api.py
@@ -11,5 +11,5 @@ router = Router(tags=["foods"])
 
 @router.get("/search", operation_id="searchFoods", response=List[FoodSchema])
 def search_foods(request, query: str, limit: int = 5, offset: int = 0):
-    results = Food.objects.filter(Q(product_name__icontains=query) | Q(brands__icontains=query) | Q(code__startswith=query) | Q(categories__icontains=query))[offset:offset+limit]
+    results = Food.objects.filter(Q(product_name__icontains=query) | Q(brands__icontains=query) | Q(code__startswith=query) | Q(categories__icontains=query)).order_by('-unique_scans_n')[offset:offset+limit]
     return results
