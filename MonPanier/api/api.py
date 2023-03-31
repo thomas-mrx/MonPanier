bbafb68453f2609f78d831a0f07e4ec2a196f3f0
thomas-mrx <thomas.moreaux@icloud.com>
2023-03-31 09:24:21 +0200

    feat: add api foods smart search route (code, product name, brands, category)

--- MonPanier/api/api.py
+++ MonPanier/api/api.py
@@ -1,12 +1,14 @@
 from ninja import NinjaAPI
 from MonPanier.api.carts.api import router as carts_router
 from MonPanier.api.products.api import router as products_router
+from MonPanier.api.foods.api import router as foods_router
 from MonPanier.api.auth.api import router as auth_router
 from ninja.security import django_auth
 
 api = NinjaAPI(title="MonPanier", version="0.0.7", csrf=True, auth=django_auth)
 api.add_router("/carts", carts_router)
 api.add_router("/products", products_router)
+api.add_router("/foods", foods_router)
 api.add_router("/auth", auth_router)
 
 urls = api.urls
---
+++ MonPanier/api/foods/api.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from django.db.models import Q
+from ninja import Router
+
+from MonPanier.api.foods.models import Food
+from MonPanier.api.foods.schemas import FoodSchema
+
+router = Router(tags=["foods"])
+
+
+@router.get("/search", operation_id="searchFoods", response=List[FoodSchema])
+def search_foods(request, query: str, limit: int = 5, offset: int = 0):
+    results = Food.objects.filter(Q(product_name__icontains=query) | Q(brands__icontains=query) | Q(code__startswith=query) | Q(categories__icontains=query))[offset:offset+limit]
+    return results
