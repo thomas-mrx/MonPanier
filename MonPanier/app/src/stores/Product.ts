fb3df2011e11ded13d5a3a189bc4112b8e4f1075
VictorTruong18 <victor.truong18@gmail.com>
2023-04-02 21:20:33 +0200

    feat: add cart details and product details

---
+++ MonPanier/app/src/stores/Product.ts
@@ -0,0 +1,16 @@
+import Store from '../scripts/Store';
+import { ProductSchema } from '../api';
+
+const STORE_NAME = 'product';
+const STORE_DATA: {
+  product: ProductSchema,
+  updateProduct: (product: ProductSchema) => void,
+} = {
+  product: {} as ProductSchema,
+
+  updateProduct(product: ProductSchema) {
+    this.product = product;
+  },
+};
+
+export default new Store(STORE_NAME, STORE_DATA) as unknown as typeof STORE_DATA;
--- MonPanier/app/src/stores/Routes.ts
+++ MonPanier/app/src/stores/Routes.ts
@@ -1,19 +1,23 @@
 import Store from '../scripts/Store';
 import Backend from '../scripts/Backend';
 import Cart from './Cart';
+import Product from './Product';
 import Stats from '../scripts/Stats';
+import { ProductSchema } from '../api';
 
 interface Route {
   pattern: RegExp,
   args: { [key: string]: string },
   onInit?: () => void,
 }
+
 interface Tab {
   icon: string,
   name: string,
   link: string,
   routes: Route[],
 }
+
 const STORE_NAME = 'routes';
 const STORE_DATA: {
   tabs: Tab[],
@@ -64,7 +68,9 @@ const STORE_DATA: {
         pattern: /^\/carts\/(?<id>[0-9]+)\/(?<product>[0-9]+)$/,
         args: { id: '', product: '' },
         onInit() {
-          console.log(this.args);
+          const product = JSON.parse(JSON.stringify(Cart.cart.products)).find((p : ProductSchema) => String(p.id) === this.args.product);
+
+          Product.updateProduct(product);
         },
       }],
     },
@@ -97,7 +103,7 @@ const STORE_DATA: {
 
   loadRoute(url: string, updateHistory = true) {
     this.tabs.forEach((tab: Tab, index: number) => {
-      tab.routes.forEach((route: Route, indexRoute:number) => {
+      tab.routes.forEach((route: Route, indexRoute: number) => {
         const match = route.pattern.exec(url);
         if (match) {
           this.activeTab = index;
